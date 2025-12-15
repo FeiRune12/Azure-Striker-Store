2ef5705bc44b95a1253e49a41281e1e37a6a4561
Fei <147347262+FeiRune12@users.noreply.github.com>
2025-12-14 21:00:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.body.classList.add("no-scroll");
+
 // Scroll suave para links de navegação
 document.querySelectorAll('nav a').forEach(link => {
   link.addEventListener('click', function(e) {
@@ -19,30 +21,45 @@ cards.forEach(card => {
   });
 });
 
-function abrirModal(imagem, titulo, descricao, preco, mensagem) {
+function abrirModal(img, titulo, desc, preco, mensagem) {
   const overlay = document.getElementById("modal-overlay");
 
-  if (!overlay) {
-    console.error("modal-overlay não encontrado");
-    return;
-  }
-
-  document.getElementById("modal-img").src = imagem;
+  document.getElementById("modal-img").src = img;
   document.getElementById("modal-title").textContent = titulo;
-  document.getElementById("modal-desc").textContent = descricao;
+  document.getElementById("modal-desc").textContent = desc;
   document.getElementById("modal-price").textContent = preco;
 
-  const texto = encodeURIComponent(
-    `Olá! Quero encomendar:\n${titulo}\n\nTema:\nPrazo:\nReferências:`
-  );
+  const link = document.getElementById("modal-link");
+  link.href = `https://wa.me/5511966076576?text=${encodeURIComponent(mensagem)}`;
 
-  document.getElementById("modal-link").href =
-    "https://wa.me/5511966076576?text=" + texto;
-
-  overlay.style.display = "flex";
+  overlay.classList.add("active");
+  document.body.classList.add("modal-open");
 }
 
 function fecharModal() {
   const overlay = document.getElementById("modal-overlay");
-  if (overlay) overlay.style.display = "none";
+
+  overlay.classList.remove("active");
+  document.body.classList.remove("modal-open");
 }
+
+
+// Selecionando o botão "Ver Serviços" e a seção da Hero
+const btnServicos = document.getElementById('btn-servicos');
+const heroSection = document.getElementById('inicio');
+
+document.addEventListener("DOMContentLoaded", () => {
+const introOverlay = document.getElementById("intro-overlay");
+const btnVerServicos = document.getElementById("intro-ver-servicos");
+
+btnVerServicos.addEventListener("click", () => {
+  introOverlay.classList.add("hidden");
+  document.body.classList.remove("no-scroll");
+});
+
+
+    
+    document
+      .getElementById("servicos")
+      .scrollIntoView({ behavior: "smooth" });
+  });
